52bb9079c71c6a8ee03becbee3862490dfa66408
danielsheetrit <danielshitrit022@gmail.com>
2021-07-27 12:15:40 +0300

    12:15

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import '../styles/globals.css'
+import '../styles/basic/globals.scss'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
---
+++ pages/events/[slug].js
@@ -0,0 +1,7 @@
+export default function costumeEevent() {
+    return (
+        <div>
+            <h1>balagan</h1>
+        </div>
+    )
+}
