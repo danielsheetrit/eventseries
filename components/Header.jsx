41bd4aa666c37357f6cefb4dfdced3e4ea72f789
danielsheetrit <danielshitrit022@gmail.com>
2021-08-02 16:20:42 +0300

    16:20

--- components/Header.jsx
+++ components/Header.jsx
@@ -5,13 +5,16 @@ export default function Header() {
         <header className="container">
             <div className="flex space-between align-center">
                 <div className="logo"></div>
-                <nav>
-                    <ul className="flex">
+                <nav className="flex ">
+                    <ul className="flex align-center">
                         <li>
                             <Link href='/'>Home</Link>
                         </li>
                         <li>
                             <Link href="/about">About</Link>
+                        </li>
+                        <li>
+                            <Link href="/about">Sign up</Link>
                         </li>
                     </ul>
                 </nav>
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,18 @@
 export default function Home() {
   return (
-    <section className="container">
-      <h1>home</h1>
+    <section className="home container">
+      <div className="hero flex column align-center justify-center">
+        <h3>
+          All the crazy sh*t i did tonight, <br />
+          those will be the best memories
+        </h3>
+        <span>
+          ▪ David Guetta ft Kid Cudi
+        </span>
+        <button>
+          Find a party for tonight ➝
+        </button>
+      </div>
     </section>
   )
 }
